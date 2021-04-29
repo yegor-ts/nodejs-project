29265133e3aab9e9b8d4c7ed930a641421f2bb13
Yegor Tsukanov <69nonesons@gmail.com>
2021-04-29 11:29:23 +0300

    Add filename property to user class

--- app/modules/user/user.model.js
+++ app/modules/user/user.model.js
@@ -4,27 +4,32 @@ const {pathToDir} = require('../../lib/config');
 
 class User {
     constructor() {
+        this.fileName = 'user'
         this.repository = new DbFileManager(pathToDir, 'user');
     }
 
     async save(entity) {
-        return await this.repository.createEntity('user', entity);
+        return await this.repository.createEntity(this.fileName, entity);
     }
 
     async findOne(id) {
-        return await this.repository.getEntity('user', id);
+        return await this.repository.getEntity(this.fileName, id);
     }
 
     async findAll() {
-        return await this.repository.getAll('user');
+        return await this.repository.getAll(this.fileName);
     }
 
     async updateOne(id, dataForUpdate) {
-        return await this.repository.updateEntity('user', id, dataForUpdate);
+        return await this.repository.updateEntity(this.fileName, id, dataForUpdate);
     }
 
     async deleteOne(id) {
-        return await this.repository.deleteEntity('user', id);
+        return await this.repository.deleteEntity(this.fileName, id);
+    }
+
+    getName() {
+        return this.fileName;
     }
 }
 
